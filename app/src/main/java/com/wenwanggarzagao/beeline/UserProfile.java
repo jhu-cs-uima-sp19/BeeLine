b093fd7d6391dbd769987abed87e124975c6cf4f
unknown <bronte.wen98@gmail.com>
2019-04-13 00:10:52 -0400

    playing around with colors, user profile

--- app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
@@ -2,6 +2,7 @@ package com.wenwanggarzagao.beeline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -9,7 +10,15 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        getSupportActionBar().setTitle(R.string.title_activity_profile);
+
+
+        TextView nameTextView = (TextView) findViewById(R.id.uprof_name);
+        nameTextView.setText("Joe Ansel Ensky");
+        TextView descripTextView = (TextView) findViewById(R.id.uprof_descrip);
+        descripTextView.setText("Description Here");
     }
+
 
 
 }
