fe98fb2d54862f4d693f1fde558349cc3522536f
unknown <bronte.wen98@gmail.com>
2019-04-13 01:49:03 -0400

    more messing around with colors and styles, this took too long save me

--- app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
@@ -2,6 +2,8 @@ package com.wenwanggarzagao.beeline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 public class UserProfile extends AppCompatActivity {
@@ -10,8 +12,9 @@ public class UserProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        getSupportActionBar().setTitle(R.string.title_activity_profile);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_uprof);
+        setSupportActionBar(toolbar);
 
         TextView nameTextView = (TextView) findViewById(R.id.uprof_name);
         nameTextView.setText("Joe Ansel Ensky");
