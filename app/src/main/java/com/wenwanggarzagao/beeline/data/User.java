ef01d429d7b48f3358619fe77dc41a6ab4b6bd1c
unknown <bronte.wen98@gmail.com>
2019-04-14 13:06:33 -0400

    user profile cancel button

--- app/src/main/java/com/wenwanggarzagao/beeline/data/User.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/data/User.java
@@ -10,19 +10,25 @@ public class User {
     private String name;
     private String username;
     private UUID id;
+    private String bio;
 
-    public static User newUser(String name, String username) {
-        return new User(UUID.randomUUID(), name, username);
+    public static User newUser(String name, String username, String bio) {
+        return new User(UUID.randomUUID(), name, username, bio);
     }
 
-    public User(UUID id, String name, String username) {
+    public User(UUID id, String name, String username, String bio) {
         this.name = name;
         this.username = username;
         this.id = id;
+        this.bio = bio;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getBio() {
+        return bio;
     }
 
     @Override
