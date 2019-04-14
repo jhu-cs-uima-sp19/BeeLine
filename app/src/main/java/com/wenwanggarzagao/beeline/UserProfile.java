586f1c3f580f362d3e5c2af578d7e1c7a0139fa0
nancy-wang <nwang22@jhu.edu>
2019-04-13 23:55:31 -0400

    add close window functionality to user profile

--- app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
@@ -4,18 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class UserProfile extends AppCompatActivity {
 
+    private static ImageButton cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
+
         Toolbar toolbar = findViewById(R.id.toolbar_uprof);
         setSupportActionBar(toolbar);
 
+        cancel = (ImageButton) findViewById(R.id.imageButton);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         TextView nameTextView = (TextView) findViewById(R.id.uprof_name);
         nameTextView.setText("Joe Ansel Ensky");
         TextView descripTextView = (TextView) findViewById(R.id.uprof_descrip);
