8766db9794a571dadc25632cb965dd8332edae33
nancy-wang <nwang22@jhu.edu>
2019-04-14 00:25:11 -0400

    minor UI fixes to find beelines

--- app/src/main/java/com/wenwanggarzagao/beeline/CreateBeeline.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/CreateBeeline.java
@@ -1,37 +1,27 @@
 package com.wenwanggarzagao.beeline;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class FindBeelines extends AppCompatActivity {
+public class CreateBeeline extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_beelines);
+        setContentView(R.layout.activity_create_beeline);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button tripDetails = findViewById(R.id.button);
-        tripDetails.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(FindBeelines.this, BeelineDetails.class);
-                startActivity(intent);
-            }
-        });
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(FindBeelines.this, CreateBeeline.class);
-                startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
