dc38ca428e1a79f8e4d01075b44a9eea71acb1c8
unknown <bronte.wen98@gmail.com>
2019-04-29 18:12:16 -0400

    user profile editing works

--- app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
+++ app/src/main/java/com/wenwanggarzagao/beeline/UserProfile.java
@@ -1,10 +1,12 @@
 package com.wenwanggarzagao.beeline;
 
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,6 +18,9 @@ import com.wenwanggarzagao.beeline.io.ResponseHandler;
 
 public class UserProfile extends AppCompatActivity {
 
+    private String userUID;
+    private SavedUserData sud;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +29,7 @@ public class UserProfile extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_uprof);
         setSupportActionBar(toolbar);
 
-        ImageButton cancel = (ImageButton) findViewById(R.id.imageButton);
+        final ImageButton cancel = (ImageButton) findViewById(R.id.exitButton);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,21 +41,82 @@ public class UserProfile extends AppCompatActivity {
         ImageView userImageView = (ImageView) findViewById(R.id.uprof_image);
         final TextView descripTextView = (TextView) findViewById(R.id.uprof_descrip);
 
+        final EditText editNameField = (EditText) findViewById(R.id.editName);
+        final EditText editBioField = (EditText) findViewById(R.id.editBio);
+
+        final ImageButton checkBtn = (ImageButton) findViewById(R.id.checkButton);
+        final ImageButton editBtn = (ImageButton) findViewById(R.id.editButton);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            String userUID = extras.getString("userUID", null);
+            userUID = extras.getString("userUID", null);
             if (userUID != null) {
                 DatabaseUtils.queryUserData(userUID, new ResponseHandler<SavedUserData>() {
                     @Override
                     public void handle(SavedUserData savedUserData) {
+                        sud = savedUserData;
                         nameTextView.setText(savedUserData.username);
                         descripTextView.setText(savedUserData.bio);
+                        if (!userUID.equals(DatabaseUtils.me.getId())) { //if not my profile, cannot edit
+                            editBtn.setVisibility(View.GONE);
+                        }
                     }
                 });
+            } // invalid user data
+            else {
+                descripTextView.setText("Could not load user data");
+                editBtn.setVisibility(View.GONE);
             }
         } else {
             descripTextView.setText("Could not load user data");
+            editBtn.setVisibility(View.GONE);
         }
+
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //view -> edit mode
+                editBtn.setVisibility(View.GONE);
+                nameTextView.setVisibility(View.GONE);
+                descripTextView.setVisibility(View.GONE);
+                cancel.setVisibility(View.GONE);
+
+                checkBtn.setVisibility(View.VISIBLE);
+                editNameField.setVisibility(View.VISIBLE);
+                editNameField.setText(nameTextView.getText());
+                editBioField.setVisibility(View.VISIBLE);
+                editBioField.setText(descripTextView.getText());
+            }
+        });
+
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // edit -> view mode
+                //save data
+                final String newName = editNameField.getText().toString();
+                final String newBio = editBioField.getText().toString();
+
+                if (sud != null) {
+                    sud.username = newName;
+                    sud.bio = newBio;
+                    DatabaseUtils.pushUserData(sud);
+                }
+                DatabaseUtils.me.setSaveData(sud);
+
+                //reset visibilities
+                editBtn.setVisibility(View.VISIBLE);
+                nameTextView.setVisibility(View.VISIBLE);
+                nameTextView.setText(newName);
+                descripTextView.setVisibility(View.VISIBLE);
+                descripTextView.setText(newBio);
+                cancel.setVisibility(View.VISIBLE);
+
+                checkBtn.setVisibility(View.GONE);
+                editNameField.setVisibility(View.GONE);
+                editBioField.setVisibility(View.GONE);
+            }
+        });
+
     }
 
 
